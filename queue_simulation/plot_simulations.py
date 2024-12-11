9c571eefc72e37a8ff9cce0d71fad18ff8ac1217
Daniel Silva <returndaniels@gmail.com>
2024-12-11 10:32:37 -0300

    Rename test_simulations.py to plot_simulations.py and import simulation configurations from constants

--- queue_simulation/plot_simulations.py
+++ queue_simulation/plot_simulations.py
@@ -1,12 +1,6 @@
 import matplotlib.pyplot as plt
 from simulation import Sistema
-
-# Configurações para cada situação
-situacoes = [
-    {"tempos_servico": [0.4, 0.6, 0.95], "distrib_servico": ["deterministico", "deterministico", "deterministico"]},
-    {"tempos_servico": [(0.1, 0.7), (0.1, 1.1), (0.1, 1.8)], "distrib_servico": ["uniforme", "uniforme", "uniforme"]},
-    {"tempos_servico": [0.4, 0.6, 0.95], "distrib_servico": ["exponencial", "exponencial", "exponencial"]}
-]
+from constants import situacoes
 
 # Simular e coletar métricas
 lambda_chegada = 2
